ee5e36554630c81e011db6d65f927cbb90189924
Jan Müller <muller.jan@gmail.com>
2019-11-29 09:06:36 +0100

    first version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://www.microsip.org/downloads/?file=MicroSIP-Lite-3.19.22.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'microsip-lite*'
+  checksum      = '11ee3f204ed3a29d9e0b6ce8a3da3e60b5bd3e923bf2f31777eeb6ec13a36d02'
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
